e2a945aeda226e5df0ef148d4354beaad6c11b34
yometo <blackymt@vip.qq.com>
2020-07-22 16:11:13 +0900

    変数

--- Assets/Hennsuu.cs
+++ Assets/Hennsuu.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class Hennsuu : MonoBehaviour
 {
+    int vx = 20;
+    //順次処理、分岐処理、繰り返し処理
+    public void ensyu71()
+    {
+        int vx = 5;
+        Debug.Log(vx);
+
+        vx = 10;
+        Debug.Log(vx);
+    }
+
+    public void ensyu72()
+    {
+        Debug.Log(vx);
+    }
+
+    public void ensyu74()
+    {
+        vx += 10;
+        Debug.Log(vx);
+    }
+        
+
+    int a = 10;
+
     // Start is called before the first frame update
     public void Proc()
     {
-        int a = 10;
         float fw = 3.14f;
         string str = "こんにちは";
         bool bl = true;
 
-        Debug.Log(a);
+        Debug.Log(a+a);
         Debug.Log(fw);
         Debug.Log(str);
         Debug.Log(bl);
